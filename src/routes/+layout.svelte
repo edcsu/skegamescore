2abe9d854ca182f8bd9d5d07047dcc4158efbe2b
Ssewannonda Keith Edwin <38857594+edcsu@users.noreply.github.com>
2026-01-06 00:58:06 +0300

    style: add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import Navbar from '$lib/components/ui/navbar/navbar.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
 <main>
+	<Navbar />
 	{@render children()}
 </main>
