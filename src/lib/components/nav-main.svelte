d955de0eb1bd5e3ae05da2eb0743dccaad84cf75
Ssewannonda Keith Edwin <38857594+edcsu@users.noreply.github.com>
2026-01-10 01:17:03 +0300

    refactor: dashboard layout

---
+++ src/lib/components/nav-main.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { CirclePlusIcon } from '@lucide/svelte';
+
+	let { items }: { items: { title: string; url: string; icon?: any }[] } = $props();
+</script>
+
+<Sidebar.Group>
+	<Sidebar.GroupContent class="flex flex-col gap-2">
+		<Sidebar.Menu>
+			<Sidebar.MenuItem class="flex items-center gap-2">
+				<Sidebar.MenuButton
+					class="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+					tooltipContent="create article"
+				>
+					<a href="/dashboard/articles/new" class="flex items-center gap-2">
+						<CirclePlusIcon class="size-5!" />
+						<span class="text-base font-semibold">New Article</span>
+					</a>
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+		<Sidebar.Menu>
+			{#each items as item (item.title)}
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton tooltipContent={item.title}>
+						{#snippet child({ props })}
+							<a href={item.url} {...props}>
+								{#if item.icon}
+									<item.icon />
+								{/if}
+								<span>{item.title}</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			{/each}
+		</Sidebar.Menu>
+	</Sidebar.GroupContent>
+</Sidebar.Group>
