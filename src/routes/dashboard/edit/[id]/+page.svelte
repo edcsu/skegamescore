c6ca928406fed73b3edd14310c201d0a05d5ea68
Ssewannonda Keith Edwin <38857594+edcsu@users.noreply.github.com>
2026-01-10 15:40:38 +0300

    style: show edit info

--- src/routes/dashboard/edit/[id]/+page.svelte
+++ src/routes/dashboard/edit/[id]/+page.svelte
@@ -10,7 +10,7 @@
 	<div class="@container/main flex flex-1 flex-col gap-2">
 		<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 			<div class="px-4 lg:px-6">
-				<ArticleForm formType="edit" articleform={form} />
+				<ArticleForm formType="edit" articleform={{ ...data.article, form }} />
 			</div>
 		</div>
 	</div>
